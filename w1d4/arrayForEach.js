504864d353d37f22bb0b3a4eddc65f56c1d0332e
Erik <erikeh1@gmail.com>
2018-09-27 17:09:15 +0000

    finished first assignment

--- w1d4/arrayForEach.js
+++ w1d4/arrayForEach.js
@@ -1,19 +1,11 @@
-The second argument/parameter is expected to be a function
 function findWaldo(arrayOfNames, found) {
-  for (var i = 0; i < arrayOfNames.length; i++) {
-    if (arrayOfNames[i] === "Waldo") {
-      found(i);   // this will send i into foundIndex
+  arrayOfNames.forEach(function(element, i){
+    if(element === "Waldo"){
+      found(i);
     }
-  }
+  }) // this hints to a callback
 }
 function actionWhenFound(foundIndex) {
    console.log("Found Waldo at index " + foundIndex + "!");
 }
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-
-
-
-
-
-
